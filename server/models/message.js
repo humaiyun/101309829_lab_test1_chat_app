0185acb8c1138ad496223edd520a74da489c9e3a
Humaiyun Uddin <uddin513@msn.com>
2023-02-06 12:42:57 -0500

    create message schema

---
+++ server/models/message.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const message = new mongoose.Schema({
+    from_user: {
+        type: String,
+        trim: true,
+    },
+    room: {
+        type: String,
+        trim: true,
+        maxLength: 50
+    },
+    message: {
+        type: String
+    },
+    date_sent: {
+        type: Date,
+        default: Date.now()
+    },
+});
+
+export default mongoose.model("Message", message);
